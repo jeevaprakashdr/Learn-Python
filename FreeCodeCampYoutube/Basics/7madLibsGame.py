1f4181466cfccf5c8c29516021b10c03629cc330
jeeva <jeeva.ramu@codurance.com>
2022-10-07 22:53:15 +0100

    mad lib game

---
+++ FreeCodeCampYoutube/Basics/7madLibsGame.py
@@ -0,0 +1,6 @@
+color = input("Enter a colour: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print(f"Roses are {color}")
+print(f"{plural_noun} are blue")
+print(f"I love {celebrity}")
