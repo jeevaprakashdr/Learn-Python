8633cf50bc7e943befc273669867042db1b79152
jeeva <jeeva.ramu@codurance.com>
2022-10-07 21:34:47 +0100

    working with string

--- FreeCodeCampYoutube/Basics/workingWithStrings.py
+++ FreeCodeCampYoutube/Basics/workingWithStrings.py
@@ -1,1 +1,28 @@
-print("python academy")
+print("Python Academy")
+print("Python\nAcademy\n")
+print("Python\"Academy")
+
+phrase = "Python Academy"
+print(phrase)
+print(phrase + "is cool")
+
+
+print(phrase.lower())
+print(phrase.upper())
+
+print(phrase.isupper())
+print(phrase.islower())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+
+print(phrase[0])
+print(phrase[1])
+print(phrase[2])
+
+print(phrase.index("P"))
+print(phrase.index("n"))
+print(phrase.index("Academy"))
+
+print(phrase.replace("Academy", "School"))
