2483bf958c3189ddae755102f31f3d67929e7ac7
jeeva <jeeva.ramu@codurance.com>
2022-10-07 21:49:42 +0100

    working with numbers

---
+++ FreeCodeCampYoutube/Basics/workingWithNumbers.py
@@ -0,0 +1,38 @@
+from math import *
+
+print(2)
+print(-2)
+
+print(2.00023)
+print(-2.00023)
+
+
+print(1 + 1)
+print(2 * 3)
+print(2 * 3 + 5)
+print(2 * (3 + 5))
+
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+print(f"my fav number {my_num}")
+
+negative_number = -5
+print(abs(negative_number))
+
+print(pow(my_num, 2))
+print(max(4,546))
+print(max([12,132,34]))
+
+print(min(4, 546))
+print(min([12, 132, 34]))
+
+print(round(3.2))
+print(round(3.6))
+
+print(floor(3.7))
+print(ceil(3.7))
+
+print(sqrt(36))
+print(floor(sqrt(36)))
