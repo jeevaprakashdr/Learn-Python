d19037407eb7c64e9e305c94785bf1b549c1bde2
jeeva <jeeva.ramu@codurance.com>
2022-10-07 21:12:08 +0100

    introduced variables

--- FreeCodeCampYoutube/Basics/variableAndDataTypes.py
+++ FreeCodeCampYoutube/Basics/variableAndDataTypes.py
@@ -1,4 +1,6 @@
-print("There once was a man names George.")
-print("he was 25 years old.")
-print("He really liked the name George.")
-print("but did not like being 35.")
+charecter_name = "John"
+charecter_age = 35
+print(f"There once was a man names {charecter_name}.")
+print(f"he was {charecter_age} years old.")
+print(f"He really liked the name {charecter_name}.")
+print(f"but did not like being {charecter_age}.")
