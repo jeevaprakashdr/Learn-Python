2a12c4ca4de53752908816001fd7f05d20c453c8
jeeva <jeeva.ramu@codurance.com>
2022-10-07 22:59:33 +0100

    lists basics

---
+++ FreeCodeCampYoutube/Basics/8lists_basics.py
@@ -0,0 +1,19 @@
+friends = ["c#", "javascript", "python", "rust", "go", "nodejs"]
+print(friends)
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+
+print(friends[-2])
+print(friends[-1])
+print(friends[0])
+
+print(friends[1:])
+print(friends[2:])
+
+print(friends[2:3])
+print(friends[2:5])
+
+
+
