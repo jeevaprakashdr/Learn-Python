b276c57ebe2dd8a7b2a15e5051f03d7ae7601247
jeeva <jeeva.ramu@codurance.com>
2022-10-07 21:59:04 +0100

    basic calculator

---
+++ FreeCodeCampYoutube/Basics/basicCalculator.py
@@ -0,0 +1,8 @@
+number_1 = input("Enter A number: ")
+number_1 = float(number_1)  # int(number_1)
+number_2 = input("Enter another  number: ")
+number_2 = float(number_2)
+
+result = number_1 + number_2
+
+print(result)
