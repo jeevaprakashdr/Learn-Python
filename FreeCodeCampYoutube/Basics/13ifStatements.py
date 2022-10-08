7ce5f24fd1bfc836674b802020248b47225c88eb
jeeva <jeeva.ramu@codurance.com>
2022-10-08 13:53:36 +0100

    if statements

---
+++ FreeCodeCampYoutube/Basics/13ifStatements.py
@@ -0,0 +1,12 @@
+is_fruit = True
+is_vegitable = True
+
+if is_fruit or is_vegitable:
+    print("It is fruit or vegetable")
+else:
+    print("it is neither fruit or vegetable")
+
+if is_fruit and is_vegitable:
+    print("It is fruit and vegetable")
+else:
+    print("it is either fruit or vegetable")
